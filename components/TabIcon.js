961ca63df286d00fe11e3fac970482f7a18608ff
Mirokil Khabibullaev <50965077+Mirokil@users.noreply.github.com>
2022-05-30 22:43:58 +0100

    Add files via upload

---
+++ components/TabIcon.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { View, Image } from "react-native"
+import { colors } from "../constants"
+
+const TabIcon = ({ focused, icon }) => {
+    return (
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <Image
+                source={icon}
+                resizeMode="contain"
+                style={{
+                    width: 25,
+                    height: 25,
+                    tintColor: focused ? colors.red : colors.gray
+                }}
+            />
+        </View>
+    )
+}
+
+export default TabIcon;
